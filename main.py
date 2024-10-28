890aa7312aa7ad7f06dd8bf9ca7f10cfef54f4d0
2zm00 <ljm010301@naver.com>
2024-10-28 13:46:40 +0900

    test route, add navbar, pages

--- main.py
+++ main.py
@@ -1,4 +1,62 @@
+import time
+import numpy as np
+import pandas as pd
 import streamlit as st
+from menu import menu
+
+
+# Initialize st.session_state.role to None
+if "role" not in st.session_state:
+    st.session_state.role = None
+
+# Retrieve the role from Session State to initialize the widget
+st.session_state._role = st.session_state.role
+
+def set_role():
+    # Callback function to save the role selection to Session State
+    st.session_state.role = st.session_state._role
+
+
+# Selectbox to choose role
+st.selectbox(
+    "Select your role:",
+    [None, "user", "admin", "super-admin"],
+    key="_role",
+    on_change=set_role,
+)
+menu() # Render the dynamic menu!
+
 
 
 st.title('My First App')
+st.write("1234")
+
+
+_LOREM_IPSUM = """
+Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
+
+
+def stream_data():
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+    yield pd.DataFrame(
+        np.random.randn(5, 10),
+        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+    )
+
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+        
+
+
+
+
+if st.button("NASA"):
+    st.write_stream(stream_data)
+    st.image("nasa.jpg", caption="images from NASA")
